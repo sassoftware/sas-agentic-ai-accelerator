310e99394314c0cb48cdf60952c0a26837dd36b3
David Weik <geekupyourlife@gmail.com>
2025-10-21 15:41:16 +0200

    Update config
    Signed-off-by: David Weik <geekupyourlife@gmail.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://sassoftware.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/sas-agentic-ai-accelerator/'
-  baseUrl: "/sas-portal-framework-for-sas-viya/",
+  baseUrl: "/sas-agentic-ai-accelerator/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
